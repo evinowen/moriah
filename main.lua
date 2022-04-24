1860a9de8cbdb93565e5a81996835f089abcbbc4
Evin Owen <github@evinowen.net>
2022-04-23 19:24:18 -0700

    Add logic for Eggs

--- main.lua
+++ main.lua
@@ -10,6 +10,16 @@ function Moriah:print(message)
   Isaac.ConsoleOutput("[Moriah] "..message.."\n")
 end
 
+function Moriah:contains(table, search)
+  for index, value in ipairs(table) do
+      if value == search then
+          return true
+      end
+  end
+
+  return false
+end
+
 function Moriah:merge(base, addition)
   for key, value in pairs(addition) do
     base[key] = value
@@ -56,7 +66,8 @@ function Moriah:stage()
     lunchbox_held = {},
     lunchbox_filled = {},
     lunchbox_opened = {},
-    lunchbox_release = {}
+    lunchbox_release = {},
+    eggs_held = {}
   }
 end
 
@@ -81,6 +92,7 @@ function Moriah:reset()
     Moriah.Data.lunchbox_held[Moriah:tag(player.ControllerIndex)] = false
     Moriah.Data.lunchbox_filled[Moriah:tag(player.ControllerIndex)] = false
     Moriah.Data.lunchbox_opened[Moriah:tag(player.ControllerIndex)] = false
+    Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)] = -1
   end
 end
 
@@ -221,6 +233,138 @@ Moriah:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Moriah.evaluate_cache, CacheF
 Moriah:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, Moriah.create_packed_lunchbox, packed_lunchbox_entity_id)
 Moriah:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, Moriah.follow_packed_lunchbox, packed_lunchbox_entity_id)
 
+local egg_tear_id = Isaac.GetEntityVariantByName("Chicken Egg Tear")
+
+local eggs_id = Isaac.GetItemIdByName("Eggs")
+local eggs_5_id = Isaac.GetItemIdByName("Eggs (5)")
+local eggs_4_id = Isaac.GetItemIdByName("Eggs (4)")
+local eggs_3_id = Isaac.GetItemIdByName("Eggs (3)")
+local eggs_2_id = Isaac.GetItemIdByName("Eggs (2)")
+local eggs_1_id = Isaac.GetItemIdByName("Eggs (1)")
+local eggs_0_id = Isaac.GetItemIdByName("Egg Carton")
+
+local egg_id_table = {
+  eggs_id,
+  eggs_5_id,
+  eggs_4_id,
+  eggs_3_id,
+  eggs_2_id,
+  eggs_1_id,
+  eggs_0_id,
+}
+
+function Moriah:hold_eggs(item_id, _, player, _, _, _)
+  if not Moriah:contains(egg_id_table, item_id) then
+    return
+  end
+
+  if Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)] < 0 then
+
+    if item_id == eggs_id then
+      Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)] = 6
+    elseif item_id == eggs_5_id then
+      Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)] = 5
+    elseif item_id == eggs_4_id then
+      Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)] = 4
+    elseif item_id == eggs_3_id then
+      Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)] = 3
+    elseif item_id == eggs_2_id then
+      Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)] = 2
+    elseif item_id == eggs_1_id then
+      Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)] = 1
+    elseif item_id == eggs_0_id then
+      Isaac.ConsoleOutput("[Moriah] Egg Carton!\n")
+      player:RemoveCollectible(eggs_0_id)
+      player:AddHearts(2)
+      return {
+        Discharge = true,
+        Remove = true,
+        ShowAnim = true,
+      }
+    end
+
+    player:AnimateCollectible(item_id, "LiftItem", "PlayerPickup")
+  else
+    Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)] = -1
+
+    player:AnimateCollectible(item_id, "HideItem", "PlayerPickup")
+  end
+
+  return {
+    Discharge = false,
+    Remove = false,
+    ShowAnim = false,
+  }
+end
+
+function Moriah:render_eggs(player)
+  local held_eggs = Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)]
+
+  if held_eggs < 0 then
+    return
+  end
+
+  if Input.IsActionPressed(ButtonAction.ACTION_SHOOTLEFT, player.ControllerIndex)
+  or Input.IsActionPressed(ButtonAction.ACTION_SHOOTRIGHT, player.ControllerIndex)
+  or Input.IsActionPressed(ButtonAction.ACTION_SHOOTUP, player.ControllerIndex)
+  or Input.IsActionPressed(ButtonAction.ACTION_SHOOTDOWN, player.ControllerIndex) then
+    Moriah:print("Throw Egg")
+    Moriah.Data.eggs_held[Moriah:tag(player.ControllerIndex)] = -1
+
+    local fire_egg = false
+
+    if held_eggs == 6 then
+      player:AnimateCollectible(eggs_id, "HideItem", "PlayerPickup")
+      player:RemoveCollectible(eggs_id)
+      player:AddCollectible(eggs_5_id)
+      fire_egg = true
+    elseif held_eggs == 5 then
+      player:AnimateCollectible(eggs_5_id, "HideItem", "PlayerPickup")
+      player:RemoveCollectible(eggs_5_id)
+      player:AddCollectible(eggs_4_id)
+      fire_egg = true
+    elseif held_eggs == 4 then
+      player:AnimateCollectible(eggs_4_id, "HideItem", "PlayerPickup")
+      player:RemoveCollectible(eggs_4_id)
+      player:AddCollectible(eggs_3_id)
+      fire_egg = true
+    elseif held_eggs == 3 then
+      player:AnimateCollectible(eggs_3_id, "HideItem", "PlayerPickup")
+      player:RemoveCollectible(eggs_3_id)
+      player:AddCollectible(eggs_2_id)
+      fire_egg = true
+    elseif held_eggs == 2 then
+      player:AnimateCollectible(eggs_2_id, "HideItem", "PlayerPickup")
+      player:RemoveCollectible(eggs_2_id)
+      player:AddCollectible(eggs_1_id)
+      fire_egg = true
+    elseif held_eggs == 1 then
+      player:AnimateCollectible(eggs_1_id, "HideItem", "PlayerPickup")
+      player:RemoveCollectible(eggs_1_id)
+      player:AddCollectible(eggs_0_id)
+      fire_egg = true
+    end
+
+    player:DischargeActiveItem()
+
+    if fire_egg then
+      local entity = Isaac.Spawn(EntityType.ENTITY_TEAR, egg_tear_id, 0, player.Position, player:GetAimDirection() * 7, player)
+      local tear = entity:ToTear()
+      tear:AddTearFlags(TearFlags.TEAR_KNOCKBACK)
+      tear:AddTearFlags(TearFlags.TEAR_BAIT)
+
+      tear.FallingSpeed = -8
+      tear.FallingAcceleration = 0.05
+
+      local sprite = tear:GetSprite()
+      sprite:Play(sprite:GetDefaultAnimation(), true)
+      sprite.Scale = Vector(0.55, 0.55)
+    end
+  end
+end
+
+Moriah:AddCallback(ModCallbacks.MC_USE_ITEM, Moriah.hold_eggs)
+
 function Moriah:render()
   if not (Moriah.Data and Moriah.Data.initalized) then
     Moriah:start(true)
@@ -232,6 +376,7 @@ function Moriah:render()
     local player = Game():GetPlayer(i)
 
     Moriah:render_lunchbox(player)
+    Moriah:render_eggs(player)
   end
 end
 
