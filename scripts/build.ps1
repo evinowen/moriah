b2958943f6597b5dd255756a63cba18ff8109659
Evin Owen <git@evinowen.net>
2022-05-26 15:02:42 -0700

    Refactor build.ps1 script

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,59 +1,60 @@
 $script_path = Split-Path $MyInvocation.MyCommand.Path -Parent
+$root_path = Resolve-Path "$script_path\.."
 
-$root_path = Resolve-Path "${script_path}\.."
-
-$source_path = Resolve-Path "${root_path}\src"
-
-if (Test-Path "${root_path}\build") {
-  Remove-Item -Recurse -Force "${root_path}\build"
+if (Test-Path "$root_path\build") {
+  Remove-Item -Recurse -Force "$root_path\build"
 }
 
-New-Item -Force "${root_path}\build" -ItemType Directory
+New-Item -Force "$root_path\build" -ItemType Directory
+$build_path = Resolve-Path "$root_path\build"
 
-$build_path = Resolve-Path "${root_path}\build"
+New-Item -Force "$build_path\main.lua" -ItemType File
+$main_path = Resolve-Path "$build_path\main.lua"
 
-New-Item -Force "${build_path}\content" -ItemType Directory
+function Include-Raw-Directory {
+  param (
+    $path
+  )
 
-Copy-Item "${root_path}\content"   -Recurse -Filter "*.xml"  -Destination "$build_path"
-Copy-Item "${root_path}\content" -Recurse -Filter "*.anm2" -Destination "$build_path"
-Copy-Item "${root_path}\content" -Recurse -Filter "*.png"  -Destination "$build_path"
-Copy-Item "${root_path}\content" -Recurse -Filter "*.wav"  -Destination "$build_path"
+  New-Item -Force "$build_path\$path" -ItemType Directory
 
-New-Item -Force "${build_path}\resources" -ItemType Directory
-
-Copy-Item "${root_path}\resources"   -Recurse -Filter "*.xml"  -Destination "$build_path"
-Copy-Item "${root_path}\resources" -Recurse -Filter "*.png"  -Destination "$build_path"
-Copy-Item "${root_path}\resources" -Recurse -Filter "*.anm2" -Destination "$build_path"
-Copy-Item "${root_path}\resources" -Recurse -Filter "*.wav"  -Destination "$build_path"
-
-New-Item -Force "${build_path}\main.lua" -ItemType File
-
-$main_path = Resolve-Path "${build_path}\main.lua"
-
-Get-Content -Encoding utf8  "${source_path}\support.lua" | Out-File -Append -Encoding utf8 "${main_path}"
-
-$items = Get-ChildItem "${source_path}\items" -include *.lua -rec
-
-foreach ($item in $items) {
-  $relative_path = $item.FullName.Substring($root_path.Length)
-  "-- ${relative_path}" | Out-File -Append -Encoding utf8 "${main_path}"
-  "" | Out-File -Append -Encoding utf8 "${main_path}"
-  Get-Content -Encoding utf8 $item | Out-File -Append -Encoding utf8 "${main_path}"
-  "" | Out-File -Append -Encoding utf8 "${main_path}"
-  "----" | Out-File -Append -Encoding utf8 "${main_path}"
-  "" | Out-File -Append -Encoding utf8 "${main_path}"
+  $filters = "*.xml", "*.anm2", "*.png", "*.wav"
+  foreach ($filter in $filters) {
+    Copy-Item "$root_path\$path" -Recurse -Filter "$filter" -Destination "$build_path"
+  }
 }
 
-$transformations = Get-ChildItem "${source_path}\transformations" -include *.lua -rec
+Include-Raw-Directory -path "content"
+Include-Raw-Directory -path "resources"
 
-foreach ($transformation in $transformations) {
-  $relative_path = $transformation.FullName.Substring($root_path.Length)
-  "-- ${relative_path}" | Out-File -Append -Encoding utf8 "${main_path}"
-  "" | Out-File -Append -Encoding utf8 "${main_path}"
-  Get-Content -Encoding utf8 $transformation | Out-File -Append -Encoding utf8 "${main_path}"
-  "" | Out-File -Append -Encoding utf8 "${main_path}"
-  "----" | Out-File -Append -Encoding utf8 "${main_path}"
-  "" | Out-File -Append -Encoding utf8 "${main_path}"
+function Include-Source-File {
+  param (
+    $path
+  )
+
+  Get-Content -Encoding utf8  "$root_path\$path" | Out-File -Append -Encoding utf8 "$main_path"
 }
 
-Get-Content -Encoding utf8  "${source_path}\main.lua" | Out-File -Append -Encoding utf8 "${main_path}"
+function Include-Source-Directory {
+  param (
+    $path
+  )
+
+  $files = Get-ChildItem "$root_path\$path" -include *.lua -rec
+
+  foreach ($file in $files) {
+    $relative_path = $file.FullName.Substring($root_path.Length)
+
+    "-- $relative_path" | Out-File -Append -Encoding utf8 "$main_path"
+    "" | Out-File -Append -Encoding utf8 "$main_path"
+    Get-Content -Encoding utf8 $file | Out-File -Append -Encoding utf8 "$main_path"
+    "" | Out-File -Append -Encoding utf8 "$main_path"
+    "----" | Out-File -Append -Encoding utf8 "$main_path"
+    "" | Out-File -Append -Encoding utf8 "$main_path"
+  }
+}
+
+Include-Source-File -path "src\support.lua"
+Include-Source-Directory -path "src\items"
+Include-Source-Directory -path "src\transformations"
+Include-Source-File -path "src\main.lua"
